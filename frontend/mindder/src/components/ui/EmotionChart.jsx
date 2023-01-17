d3d3c3885cb969ce4b2ba7d3971686ac23efb9d8
chanshin0 <windcj1@naver.com>
2023-01-17 17:31:36 +0900

    Feat : 메인페이지 구현

--- frontend/mindder/src/components/ui/EmotionChart.jsx
+++ frontend/mindder/src/components/ui/EmotionChart.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 
-const EmotionChart = styled.div`
+const Container = styled.div`
   margin-top:10px;
     width: 360px;
-    height: 152px;
+    height: 122px;
     max-width: 720px;
     & > * {
         :not(:last-child) {
             margin-bottom: 16px;
         }
     }
-    /* border : 1px solid black; */
+    border : 1px solid black;
     /* background-color:#7767FD; */
     border-radius:20px;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: start;
     flex-direction:column;
 `
+
+const ChartHeader = styled.h4`
+    font-size:16px;
+    font-weight:900;
+    /* color:white; */
+    position:relative;
+    top:15px;
+    right:120px;
+    margin: 0;
+`
+const chart = styled.div`
+  
+`
+
+function EmotionChart (props) {
+  return (
+    <Container>
+      <ChartHeader>마인더 통계</ChartHeader>
+    </Container>
+  )
+}
+
+export default EmotionChart;
