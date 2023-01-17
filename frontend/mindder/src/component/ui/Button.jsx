4a9df43e8d0c604ff7fc5d849f114e5c9ed41603
syhj0326 <syhj0326@naver.com>
2023-01-17 14:45:26 +0900

    하단 네비게이션바 수정

--- frontend/mindder/src/component/ui/Button.jsx
+++ frontend/mindder/src/component/ui/Button.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-    background-color: white;
+    background-color: #7767FD;
     color: black;
     padding: 8px 16px;
     font-size: 16px;
-    border-width: 1px;
-    border-radius: 25px;
-    border: 2px solid #7767FD;
+    border: 1px solid #cbc5ff;
+    border-radius: 7px;
     cursor: pointer;
     box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.3);
+    
+
 `;
+
+
 
 function Button(props) {
     const { title, onClick } = props;
@@ -20,3 +23,4 @@ function Button(props) {
 }
 
 export default Button;
+
