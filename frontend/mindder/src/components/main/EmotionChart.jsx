4c4e99846164417255a3d9a305e1f5429224cb57
yiyou00@naver.com <yiyou00@naver.com>
2023-01-26 13:28:07 +0900

    [FEAT] FeedPage 실시간 페이지 목업 구현

--- frontend/mindder/src/components/main/EmotionChart.jsx
+++ frontend/mindder/src/components/main/EmotionChart.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-// 추천 캔버스
-const ChartContainer = styled.div`
-    margin-top:1rem;
-    width: 22rem;
-    height: 10rem;
-    border-radius:20px;
-    display: flex;
-    align-items: center;
-    background-color:#e1c0c0;
-    justify-content: center;
-    flex-direction:column;
-`;
-
 const Container = styled.div`
     width: 22rem;
     height: 9rem;
@@ -30,20 +17,10 @@ const Container = styled.div`
     flex-direction:column;
 `;
 
-const ChartHeader = styled.h4`
-    font-size: 1rem;
-    font-weight: 900;
-    position:relative;
-    right:120px;
-    margin: 0;
-`
 
 function EmotionChart (props) {
   return (
-    <ChartContainer>
-      <ChartHeader>마인더 통계</ChartHeader>
       <Container></Container>
-    </ChartContainer>
   )
 }
 
--- frontend/mindder/src/router/FeedPage.jsx
+++ frontend/mindder/src/router/FeedPage.jsx
@@ -1,22 +1,114 @@
-// 라우터 폴더는 uri기준으로 각각 파일 작성
-import React from "react";
+import React, { useState }from "react";
+import "../assets/css/main.css";
 import styled from "styled-components";
+import ErrorPage from "./ErrorPage";
+import CanvasList from "../commons/list/CanvasList";
+import FeedRecoDetail from "../components/feed/FeedRecoDetail";
+import PlusIcon from "../assets/images/icon5.png";
+import BackIcon from "../assets/images/back.png";
 
 const Wrapper = styled.div`
-    /* padding: 16px; */
-    /* width: 100vw; */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    margin: 2rem 0 3rem 0;
 `;
 
+const Container = styled.div`
+    display: flex;
+    color: rgb(67, 67, 67);
+    font-weight: 600;
+`  
+
+const ContainerT = styled.div`
+    margin-bottom: 3rem;
+`  
+
+const Comment = styled.div`
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
+    font-size: 1.2rem;
+    font-weight: 500;
+`  
+
 function FeedPage(props) {
-    return (
-        <Wrapper>
-            여기는 피드전체페이지
-        </Wrapper>
-    );
+    const [number, setNumber] = useState(0);
+    const [kind, setKind] = useState(0);
+    const text = [
+        {
+          id: "1",
+          comment: "최근, 비슷한 감정을 공유했어요 ",
+        },
+        {
+          id: "2",
+          comment: '최근, 비슷한 감정색을 사용했어요 ',
+        },
+        {
+          id: "3",
+          comment: "동찬님이 이웃이 최근 공유했어요",
+        }
+      ];
+    
+    console.log(number);
+    console.log(kind);
+    
+    function changeState(s) {
+        setKind(s);
+    }
+
+    const onIncrease = () => {
+        setNumber(prevNumber => prevNumber + 1);
+    }
+
+    const onDecrease = () => {
+        setNumber(prevNumber => prevNumber - 1);
+    }
+
+    if(number == 0){
+        return (
+            <Wrapper>
+                <ContainerT>
+                    <Container>
+                        <div>{text[0].comment}</div>
+                        <button type="button" className="img_btn" onClick={() => {
+                  onIncrease();
+                  changeState(0);
+                  }}><img  src={PlusIcon}/></button>
+                    </Container>
+                    <CanvasList ></CanvasList>
+                </ContainerT>
+                <ContainerT>
+                    <Container>
+                        <div>{text[1].comment}</div>
+                        <button type="button" className="img_btn" onClick={() => {
+                  onIncrease();
+                  changeState(1);
+                  }}><img  src={PlusIcon}/></button>
+                    </Container>
+                    <CanvasList></CanvasList>
+                </ContainerT>
+                <ContainerT>
+                    <Container>
+                        <div>{text[2].comment}</div>
+                        <button type="button" className="img_btn" onClick={() => {
+                  onIncrease();
+                  changeState(2);
+                  }}><img  src={PlusIcon}/></button>
+                    </Container>
+                    <CanvasList></CanvasList>
+                </ContainerT>
+            </Wrapper>
+        );
+    } else  if(number == 1) {
+        return (
+            <Wrapper>
+                <button type="button" className="img_btn"  onClick={onDecrease}><img id = "back_btn" src={BackIcon}/></button>
+                <Comment>{text[kind].comment}</Comment>
+                <FeedRecoDetail></FeedRecoDetail>
+            </Wrapper>
+        );
+    } else{
+        <ErrorPage></ErrorPage>
+    }
 }
 
 export default FeedPage;
