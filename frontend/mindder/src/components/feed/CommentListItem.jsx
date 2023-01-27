111de1201e7a336e6478d121a6c86256312a1b4e
이유경 <yiyou00@naver.com>
2023-01-27 13:54:36 +0900

    [FEAT]#35 - FeedPage 게시글 상세 보기 목업 구현 완료

---
+++ frontend/mindder/src/components/feed/CommentListItem.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components";
+import Imag from "../../assets/images/face1.png";
+
+const Wrapper = styled.div`
+    display: flex;
+    max-width: 21rem;
+    background: white;
+    align-items: center;
+`;
+
+const WrapperTop = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 19rem;
+    height: 4rem;
+    margin: 0 0 0 1rem;
+    background: white;
+`;
+
+const ContentText = styled.p`
+    color: rgb(67, 67, 67);
+    font-size: 0.75rem;
+    max-width: 12rem;
+    padding: 0 0 0 0;
+    margin: 0 0 -2rem 0;
+`;
+
+const ProfileText = styled.p`
+    color: rgb(67, 67, 67);
+    font-weight: 600;
+    font-size: 0.75rem;
+`;
+
+const ContextWrapper = styled.div`
+    width: 16rem;
+    max-width: 16rem;
+    display: flex;
+    align-content: flex-start;
+    justify-content: space-between;
+`;
+
+const Date = styled.div`
+    /* display: flex; */
+    font-size: 0.5rem;
+    color: rgb(67, 67, 67);
+    /* justify-content: center !important; */
+`
+
+const Image = styled.img`
+    width: 2rem;
+    height: 2rem;
+    border: 1px solid #c0c0c0;
+    border-radius: 100%;
+`
+
+function CommentListItem(props) {
+    const { comment } = props;
+
+    return (
+        <Wrapper>
+            <Image src = {Imag}></Image>
+            <WrapperTop>
+                <ContextWrapper>
+                    <ProfileText>이유경</ProfileText>
+                    <Date>2019.12.05</Date>
+                </ContextWrapper>
+            <ContentText>{comment.content}</ContentText>
+            </WrapperTop>
+        </Wrapper>
+    );
+}
+
+export default CommentListItem;
--- frontend/mindder/src/router/MainPage.jsx
+++ frontend/mindder/src/router/MainPage.jsx
@@ -1,7 +1,12 @@
 // 라우터 폴더는 uri기준으로 각각 파일 작성
 import React from "react";
+import styled from "styled-components";
 import LoginHome from "../components/main/LoginHome";
 import Home from "../components/main/Home";
+
+const Wrapper = styled.nav`
+    margin: 1rem 0 0 0;
+`;
 
 function MainPage(props) {
     if(false){
@@ -10,7 +15,9 @@ function MainPage(props) {
         );
     } else {
         return (
-            <LoginHome></LoginHome>
+            <Wrapper>
+                <LoginHome></LoginHome>
+            </Wrapper>
         );
     }
 }
