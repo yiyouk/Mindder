d7435d9924cf85840d8de0d43413fcbe0f33d1f3
chanshin0 <windcj1@naver.com>
2023-01-29 23:31:17 +0900

    리덕스 도입 및 포스트페이지 사용자화 1. redux 폴더 - store.js에서 상태관리 2. 감정태크 선택시 이미지 반영 및 store에 감정태그, 이미지src 저장 3. 감정색 선택시 이미지 반영 및 store에 감정색 저장.

--- frontend/mindder/src/components/post/PostEmocolor.jsx
+++ frontend/mindder/src/components/post/PostEmocolor.jsx
@@ -3,29 +3,12 @@ import styled from "styled-components";
 import EmoHeader from "./EmoHeader";
 import TodayEmotion from "./TodayEmotion";
 import EmoColors from "./EmoColors";
+import { Wrapper } from "./PostEmotag";
+import { CardContainer } from "./PostEmotag";
+import { Colors16 } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { userAction } from "../../redux/store";
 
-const Wrapper = styled.div`
-    /* padding: 0; */
-    width: calc(100% - 2rem);
-    height:31.5rem;
-    display: grid;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    /* border:1px solid blue; */
-`;
-
-const CardContainer = styled.div`
-  width:330px;
-  /* height:260px; */
-  /* border: 1px solid black; */
-  display:grid;
-
-  grid-template-columns: repeat(4, 1fr);
-  /* padding-right:10px; */
-  /* margin:8px auto; */
-  /* border: 1px solid black; */
-`
 const EmoColorList = styled.div`
   display:flex;
   justify-content: center;
@@ -33,94 +16,12 @@ const EmoColorList = styled.div`
   /* border: 1px solid blue; */
 `
 
-
 function PostEmocolor(props){
-  const colors = [
-    {
-      id:1,
-      name:"red",
-      code:"#F56161"
-    },
-    {
-      id:2,
-      name:"blue",
-      code:"#82AAE3"
-    },
-    {
-      id:3,
-      name:"green",
-      code:"#B6E2A1"
-    },
-    {
-      id:4,
-      name:"beige",
-      code:"#F8EDE3"
-    },
-    {
-      id:5,
-      name:"brown",
-      code:"#AD8270"
-    },
-    {
-      id:6,
-      name:"gray",
-      code:"#B2B2B2"
-    },
-    {
-      id:7,
-      name:"pink",
-      code:"#FC9CBB"
-    },
-    {
-      id:8,
-      name:"navy",
-      code:"#3A4F7A"
-    },
-    {
-      id:9,
-      name:"peach",
-      code:"#FFC7C7"
-    },
-    {
-      id:10,
-      name:"yellow",
-      code:"#F8F388"
-    },
-    {
-      id:11,
-      name:"orange",
-      code:"#FFCC80"
-    },
-    {
-      id:12,
-      name:"purple",
-      code:"#BA94D1"
-    },
-    {
-      id:13,
-      name:"white",
-      code:"#FFFFFF"
-    },
-    {
-      id:14,
-      name:"mint",
-      code:"#7FE9DE"
-    },
-    {
-      id:15,
-      name:"mauve",
-      code:"#863A6F"
-    },
-    {
-      id:16,
-      name:"black",
-      code:"#000000"
-    },
-  ]
-  
-  const defaultSrc = require("../../assets/images/face2.png")
+  const selectedSrc = useSelector((state)=>state.reducer.emotagSrc)
+  const selectedEmo = useSelector((state)=>state.reducer.todayEmotion)
+  const dispatch = useDispatch()
 
-  const [imgSrc, setImgSrc] = useState(defaultSrc)
+  const [imgSrc, setImgSrc] = useState(selectedSrc)
 
   return (
     <Wrapper>
@@ -129,8 +30,15 @@ function PostEmocolor(props){
       imgSrc={imgSrc}
       />
       <CardContainer>
-        {colors.map((color)=>(
-          <EmoColorList>
+        {Colors16.map((color)=>(
+          <EmoColorList key={color.id} id={color.name}
+          onClick={(e)=>{
+            const name = e.currentTarget.id
+            const updatedSrc = require(`../../assets/images/mindder_bear/${selectedEmo}/${name}.png`)
+            setImgSrc(updatedSrc)
+            dispatch(userAction.SAVE({selected:name, case:"emoColor"}))
+          }}
+          >
             <EmoColors
             key={color.id} colorCode={color.code} colorName={color.name} 
             />
