fd913a26f9a26aa0ee90e945d71cf7338e6b6321
abin7989 <abin7989@gmail.com>
2023-01-30 15:07:09 +0900

    [FEAT] 이미지 다운로드 젠킨스 TEST-1

--- backend/mindderStart/src/main/java/com/ssafy/mindder/file/controller/FileController.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/file/controller/FileController.java
@@ -1,12 +1,20 @@
 package com.ssafy.mindder.file.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FileCopyUtils;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +36,7 @@ public class FileController {
 			//String realPath = servletContext.getRealPath("/upload");
 //			String realPath = servletContext.getRealPath("/resources/img");
 			String today = new SimpleDateFormat("yyMMdd").format(new Date());
-			String saveFolder = filePath+"/img" + File.separator + today;
+			String saveFolder = filePath + File.separator + today;
 			File folder = new File(saveFolder);
 			if (!folder.exists())
 				folder.mkdirs();
@@ -49,5 +57,29 @@ public class FileController {
 		}
 		return "!";
 	}
-
+	@GetMapping
+	public String fileDownLoad(@Value("${file.path.upload-files}") String filePath) {
+        String saveFolder = "230130";	// 파일 경로
+        String originalFile ="temp";	// 원본 파일명(화면에 표시될 파일 이름)
+        String saveFile = "445304263920684.png";    	// 암호화된 파일명(실제 저장된 파일 이름)
+        File file = new File("/img" + File.separator  + saveFolder, saveFile);
+        //OutputStream out = response.getOutputStream();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            //FileCopyUtils.copy(fis, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null) {
+                try { 
+                    fis.close(); 
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println(fis);
+        return fis.toString();
+	}
 }
