d3c65b1beec314d474fb6152bea4096dec3425f9
Park Minji <pmj7755@naver.com>
2023-01-30 17:39:40 +0900

    Feat: mainFeed 리팩토링 완료

--- backend/mindderStart/src/main/java/com/ssafy/mindder/common/SuccessCode.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/common/SuccessCode.java
@@ -1,50 +1,42 @@
 package com.ssafy.mindder.common;
 
-import lombok.AccessLevel;
+import static com.ssafy.mindder.common.StatusCode.SUCCESS;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import static com.ssafy.mindder.common.StatusCode.SUCCESS;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessCode {
-	
-	/*
-	 * 등록/생성: CREATE,
-	 * 조회: READ,
-	 * 수정: UPDATE,
-	 * 삭제/취소: DELETE
-	 */
-	
-	// feeds
-	
-	// likes
-	CREATE_LIKE(SUCCESS, "공감 등록 성공"),
-	UPDATE_LIKE(SUCCESS, "공감 수정 성공"),
-	DELETE_LIKE(SUCCESS, "공감 취소 성공"),
-	
-	// my
-	READ_MY_FEED_LIST(SUCCESS, "내 피드 목록 조회 성공"),
-	READ_MY_SCRAP_LIST(SUCCESS, "내 스크랩 목록 조회 성공"),
-	READ_MY_FOLLOWER_LIST(SUCCESS, "팔로워 목록 조회 성공"),
-	READ_MY_FOLLOWING_LIST(SUCCESS, "팔로잉 목록 조회 성공"),
-	READ_MY_CALENDAR_LIST(SUCCESS, "월별 캘린더 목록 조회 성공"),
-	CREATE_MY_FOLLOW(SUCCESS, "팔로우 등록 성공"),
-	DELETE_MY_FOLLOW(SUCCESS, "팔로우 취소 성공"),
-	
-	// scrap
-	CREATE_SCRAP(SUCCESS, "스크랩 등록 성공"),
-	DELETE_SCRAP(SUCCESS, "스크랩 취소 성공"),
-	;
-	
-	// users
-	
-    private final StatusCode statusCode;
-    private final String message;
 
-    public int getStatus(){
-        return statusCode.getStatus();
-    }
+	/*
+	 * 등록/생성: CREATE, 조회: READ, 수정: UPDATE, 삭제/취소: DELETE
+	 */
+
+	// feeds
+	WRITE_MAIN_FEED(SUCCESS, "메인 피드 글 등록 성공"), UPDATE_MAIN_FEED(SUCCESS, "메인 피드 글 수정 성공"),
+	DELETE_MAIN_FEED(SUCCESS, "메인 피드 글 삭제 성공"), DETAIL_MAIN_FEED(SUCCESS, "메인 피드 글 상세보기 성공"),
+	NEIGHBORS_FEED_LIST(SUCCESS, "이웃 피드 글 보기 성공"), IMAGE_CRAAWLING_COLOR(SUCCESS, "이미지 크롤링 성공"),
+
+	// likes
+	CREATE_LIKE(SUCCESS, "공감 등록 성공"), UPDATE_LIKE(SUCCESS, "공감 수정 성공"), DELETE_LIKE(SUCCESS, "공감 취소 성공"),
+
+	// my
+	READ_MY_FEED_LIST(SUCCESS, "내 피드 목록 조회 성공"), READ_MY_SCRAP_LIST(SUCCESS, "내 스크랩 목록 조회 성공"),
+	READ_MY_FOLLOWER_LIST(SUCCESS, "팔로워 목록 조회 성공"), READ_MY_FOLLOWING_LIST(SUCCESS, "팔로잉 목록 조회 성공"),
+	READ_MY_CALENDAR_LIST(SUCCESS, "월별 캘린더 목록 조회 성공"), CREATE_MY_FOLLOW(SUCCESS, "팔로우 등록 성공"),
+	DELETE_MY_FOLLOW(SUCCESS, "팔로우 취소 성공"),
+
+	// scrap
+	CREATE_SCRAP(SUCCESS, "스크랩 등록 성공"), DELETE_SCRAP(SUCCESS, "스크랩 취소 성공"),;
+
+	// users
+
+	private final StatusCode statusCode;
+	private final String message;
+
+	public int getStatus() {
+		return statusCode.getStatus();
+	}
 }
--- backend/mindderStart/src/main/java/com/ssafy/mindder/feeds/controller/FeedsController.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/feeds/controller/FeedsController.java
@@ -10,8 +10,6 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.mindder.common.ErrorCode;
+import com.ssafy.mindder.common.SuccessCode;
+import com.ssafy.mindder.common.dto.ApiResponse;
 import com.ssafy.mindder.feeds.model.FeedsCrawlDto;
 import com.ssafy.mindder.feeds.model.FeedsDto;
 import com.ssafy.mindder.feeds.model.FeedsNeighborDto;
@@ -43,95 +44,129 @@ public class FeedsController {
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
 
-	@ApiOperation(value = "피드글 작성", notes = "새로운 피드의 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "메인 피드 글 작성", notes = "새로운 피드의 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PostMapping
-	public ResponseEntity<String> writeFeeds(
-			@RequestBody @ApiParam(value = "피드 정보.", required = true) FeedsDto feedsDto) throws Exception {
+	public ApiResponse<?> writeFeeds(@RequestBody @ApiParam(value = "피드 정보.", required = true) FeedsDto feedsDto)
+			throws Exception {
 		logger.info("writeArticle - 호출");
-		if (feedsService.writeFeed(feedsDto)) {
-			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		try {
+			feedsService.writeFeed(feedsDto);
+			return ApiResponse.success(SuccessCode.WRITE_MAIN_FEED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.debug("writeFeeds - 메인 피드 등록 중 에러 발생");
+			return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
 		}
-		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 
-	@ApiOperation(value = "피드글 수정", notes = "수정할 피드의 정보를 입력한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "메인 피드 글 수정", notes = "수정할 피드의 정보를 입력한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PutMapping
-	public ResponseEntity<String> modifyFeed(
-			@RequestBody @ApiParam(value = "수정할 글정보.", required = true) FeedsDto feedsDto) throws Exception {
+	public ApiResponse<?> modifyFeed(@RequestBody @ApiParam(value = "수정할 글정보.", required = true) FeedsDto feedsDto)
+			throws Exception {
 		logger.info("modifyFeed - 호출 {}", feedsDto);
 
-		if (feedsService.modifyFeed(feedsDto)) {
-			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		try {
+			feedsService.modifyFeed(feedsDto);
+			return ApiResponse.success(SuccessCode.UPDATE_MAIN_FEED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.debug("modifyFeed- 피드 글 수정 중 에러 발생 ");
+			return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
 		}
-		return new ResponseEntity<String>(FAIL, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "피드글 삭제", notes = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "메인 피드 글 삭제", notes = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@DeleteMapping("/{feedIdx}")
-	public ResponseEntity<String> deleteFeed(
-			@PathVariable("feedIdx") @ApiParam(value = "살제할 글의 글번호.", required = true) int feedIdx) throws Exception {
+	public ApiResponse<?> deleteFeed(
+			@PathVariable("feedIdx") @ApiParam(value = "삭제할 글의 글번호.", required = true) int feedIdx) throws Exception {
 		logger.info("deleteFeed - 호출");
-		if (feedsService.deleteFeed(feedIdx)) {
-			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+
+		try {
+			feedsService.deleteFeed(feedIdx);
+			return ApiResponse.success(SuccessCode.DELETE_MAIN_FEED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("deleteFeed - 피드 글 삭제 중 에러 발생");
+			return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
 		}
-		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 
-	@ApiOperation(value = "피드글 상세보기", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = FeedsParameterDto.class)
+	@ApiOperation(value = "메인 피드 글 상세보기", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = FeedsParameterDto.class)
 	@GetMapping("/{feedIdx}")
-	public ResponseEntity<FeedsParameterDto> getFeed(
+	public ApiResponse<?> getFeed(
 			@PathVariable("feedIdx") @ApiParam(value = "얻어올 글의 글번호.", required = true) int feedIdx) throws Exception {
 		logger.info("getFeed - 호출 : " + feedIdx);
-		return new ResponseEntity<FeedsParameterDto>(feedsService.getFeed(feedIdx), HttpStatus.OK);
-
+		try {
+			FeedsParameterDto feedDetail = feedsService.getFeed(feedIdx);
+			return ApiResponse.success(SuccessCode.DETAIL_MAIN_FEED, feedDetail);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("getFeed - 피드 글 상세 보기 중 에러 발생 ");
+			return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
+		}
 	}
 
-	// 댓글 리스트 조회에에에에에엥
-	@ApiOperation(value = "팔로잉하는 이웃의 피드 조회", notes = "이웃의 피드를 반환한다.", response = List.class)
+	// 팔로잉 하는 이웃의 피드 리스트 조회
+	@ApiOperation(value = "팔로잉 하는 이웃의 피드 조회", notes = "이웃의 피드를 반환한다.", response = List.class)
 	@GetMapping("neighbors/{userIdx}")
-	public ResponseEntity<List<FeedsNeighborDto>> neighborFeed(
+	public ApiResponse<?> neighborFeed(
 			@PathVariable("userIdx") @ApiParam(value = "유저 번호 ", required = true) int userIdx) throws Exception {
 		logger.info("userIdx - 호출");
-		return new ResponseEntity<List<FeedsNeighborDto>>(feedsService.neighborFeed(userIdx), HttpStatus.OK);
+		try {
+			List<FeedsNeighborDto> neighborList = feedsService.neighborFeed(userIdx);
+			return ApiResponse.success(SuccessCode.NEIGHBORS_FEED_LIST, neighborList);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.debug("neighborFeed - 팔로잉 하는 이웃의 피드 글 불러오는 중 에러");
+			return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
+		}
 	}
 
 	// 핀터레스트 이미지 크롤링
 	@ApiOperation(value = "이미지 크롤링 ", notes = "이웃의 피드를 반환한다.", response = List.class)
 	@GetMapping("/crawling/{color}")
-	public ResponseEntity<List<FeedsCrawlDto>> crawling(String color) throws Exception {
-//      // 일단 변수 반영 x, 메인 이미지만 가져와보기
-//      String url = "https://www.google.com/search?q=purple+drawing&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjW1rb2k-78AhWSFIgKHTgUD_8Q_AUoAXoECAEQAw&biw=1536&bih=746&dpr=1.25";
+	public ApiResponse<?> crawling(String color) throws Exception {
+		// 일단 변수 반영 x, 메인 이미지만 가져와보기
+//		String url = "https://www.google.com/search?q=purple+drawing&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjW1rb2k-78AhWSFIgKHTgUD_8Q_AUoAXoECAEQAw&biw=1536&bih=746&dpr=1.25";
 //
-//      Document doc = Jsoup.connect(url).get();
-//      Thread.sleep(1000);
-//      List<FeedsCrawlDto> list = new ArrayList<>();
-//      Elements links = doc.select("div.isv-r");
+//		Document doc = Jsoup.connect(url).get();
+//		Thread.sleep(1000);
+//		
+//		List<FeedsCrawlDto> list = new ArrayList<>();
+//		Elements links = doc.select("div.isv-r");
 
-		//
-		String url = "https://www.shutterstock.com/ko/search/" + color
-				+ "-draw?c3apidt=p67950521402&cr=ec&gclid=Cj0KCQiAz9ieBhCIARIsACB0oGJBB98nHvnTniAE-kjspSDdkQWfpIcWxlh0IFv7ed-Mr8cMmg1vLicaAiz5EALw_wcB&gclsrc=aw.ds&kw=%EC%9D%B4%EB%AF%B8%EC%A7%80%EB%8B%A4%EC%9A%B4&pl=PPC_GOO_KR_IG-567002744555";
+		logger.debug("crawling - 호출 : " + color);
 
-		Document doc = Jsoup.connect(url).get();
-		Thread.sleep(1000);
-		List<FeedsCrawlDto> list = new ArrayList<>();
+		try {
+			String url = "https://www.shutterstock.com/ko/search/" + color
+					+ "-draw?c3apidt=p67950521402&cr=ec&gclid=Cj0KCQiAz9ieBhCIARIsACB0oGJBB98nHvnTniAE-kjspSDdkQWfpIcWxlh0IFv7ed-Mr8cMmg1vLicaAiz5EALw_wcB&gclsrc=aw.ds&kw=%EC%9D%B4%EB%AF%B8%EC%A7%80%EB%8B%A4%EC%9A%B4&pl=PPC_GOO_KR_IG-567002744555";
 
-		Elements links = doc.select(".mui-b5j3lh-item-sstkGridItem-item");
+			Document doc = Jsoup.connect(url).get();
+			Thread.sleep(1000);
+			List<FeedsCrawlDto> list = new ArrayList<>();
 
-		int cnt = 1;
-		for (Element e : links) {
-			if (cnt % 10 != 0 && cnt <= 9) {
-				FeedsCrawlDto imgLists = new FeedsCrawlDto();
+			Elements links = doc.select(".mui-b5j3lh-item-sstkGridItem-item");
 
-				// 해당 링크 주소
-				String frontUrl = "https://www.shutterstock.com/";
-				imgLists.setUrl(frontUrl + e.select("a").get(0).attr("href"));
+			int cnt = 1;
+			for (Element e : links) {
+				if (cnt % 10 != 0 && cnt <= 9) {
+					FeedsCrawlDto imgLists = new FeedsCrawlDto();
 
-				imgLists.setImg(e.select("img").get(0).attr("src"));
-				list.add(imgLists);
+					// 해당 링크 주소
+					String frontUrl = "https://www.shutterstock.com/";
+					imgLists.setUrl(frontUrl + e.select("a").get(0).attr("href"));
+
+					imgLists.setImg(e.select("img").get(0).attr("src"));
+					list.add(imgLists);
+				}
+				cnt++;
 			}
-			cnt++;
+			return ApiResponse.success(SuccessCode.IMAGE_CRAAWLING_COLOR, list);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.debug(" crawling- 이미지 크롤링 중 에러");
+			return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
 		}
-		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 
 }
