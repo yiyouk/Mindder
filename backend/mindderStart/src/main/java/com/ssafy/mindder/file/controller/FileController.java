f9eb075ef460fddcd90806be57d8dce3a17ebf9b
abin7989 <abin7989@gmail.com>
2023-01-30 16:57:52 +0900

    [FEAT] 이미지 다운로드 젠킨스 TEST-3

--- backend/mindderStart/src/main/java/com/ssafy/mindder/file/controller/FileController.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/file/controller/FileController.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,25 +33,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.mindder.file.model.FileDto;
+import com.ssafy.mindder.file.model.service.FileService;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("/file")
 public class FileController {
 
+	@Autowired
+	private FileService fileService;
 	@PostMapping
 	public String fileUpLoad(@Value("${file.path.upload-files}") String filePath,
 			@RequestParam("upfile") MultipartFile[] files) throws Exception {
 		System.out.println(filePath);
 		if (!files[0].isEmpty()) {
-			// String realPath = servletContext.getRealPath("/upload");
-//			String realPath = servletContext.getRealPath("/resources/img");
 			String today = new SimpleDateFormat("yyMMdd").format(new Date());
 			String saveFolder = filePath + File.separator + today;
 			File folder = new File(saveFolder);
 			if (!folder.exists())
 				folder.mkdirs();
-			List<FileDto> fileInfos = new ArrayList<FileDto>();
 			for (MultipartFile mfile : files) {
 				FileDto fileInfoDto = new FileDto();
 				String originalFileName = mfile.getOriginalFilename();
@@ -61,25 +63,18 @@ public class FileController {
 					fileInfoDto.setSaveFile(saveFileName);
 					mfile.transferTo(new File(folder, saveFileName));
 				}
-				fileInfos.add(fileInfoDto);
+				fileService.addFile(fileInfoDto);
 			}
 		}
 		return "!";
 	}
 
-	@GetMapping
-//	public @ResponseBody byte[]  fileDownLoad(@Value("${file.path.upload-files}") String filePath) {
-//        System.out.println(1);
-//		String saveFolder = "230130";	// 파일 경로
-//        String originalFile ="temp";	// 원본 파일명(화면에 표시될 파일 이름)
-//        String saveFile = "888854090619500.png";    	// 암호화된 파일명(실제 저장된 파일 이름)
-//        File file = new File(filePath + File.separator  + saveFolder, saveFile);
-//        InputStream in = getClass().getResourceAsStream(filePath + "/"  + saveFolder+"/"+saveFile);
-//        return IOUtils.toByteArray(in);
-	public ResponseEntity<byte[]> getFile(@Value("${file.path.upload-files}") String filePath) {
-		String saveFolder = "230130"; // 파일 경로
-		String originalFile = "temp"; // 원본 파일명(화면에 표시될 파일 이름)
-		String saveFile = "888854090619500.png"; // 암호화된 파일명(실제 저장된 파일 이름)
+	@GetMapping("/{fileIdx}")
+	public ResponseEntity<byte[]> getFile(@Value("${file.path.upload-files}") String filePath,@PathVariable("fileIdx") String fileIdx) {
+		FileDto temp = fileService.findFile(1);
+		String saveFolder = temp.getSaveFolder(); // 파일 경로
+		String originalFile = temp.getOriginalFile(); // 원본 파일명(화면에 표시될 파일 이름)
+		String saveFile = temp.getSaveFile(); // 암호화된 파일명(실제 저장된 파일 이름)
 		File file = new File(filePath + File.separator + saveFolder, saveFile);
 		ResponseEntity<byte[]> result = null;
 		try {
--- backend/mindderStart/src/main/java/com/ssafy/mindder/file/model/service/FileService.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/file/model/service/FileService.java
@@ -1,4 +1,8 @@
 package com.ssafy.mindder.file.model.service;
 
+import com.ssafy.mindder.file.model.FileDto;
+
 public interface FileService {
+	void addFile(FileDto fileDto);
+	FileDto findFile(int fileIdx);
 }
