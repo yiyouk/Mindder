330ec5460cf0f584e40cebbbfca0c3aae65f5bb5
abin7989 <abin7989@gmail.com>
2023-01-31 14:29:54 +0900

    [FIX] user api 수정

--- backend/mindderStart/src/main/java/com/ssafy/mindder/users/controller/UsersController.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/users/controller/UsersController.java
@@ -139,7 +139,7 @@ public class UsersController {
 			int check = 0;
 			usersDto = usersService.login(usersDto);
 			System.out.println(usersDto);
-			if (usersDto != null) {
+			if (usersDto != null&& !usersDto.isDeleted()) {
 				String accessToken = jwtService.createAccessToken("useridx", usersDto.getUserIdx());
 				usersDto.setRefreshToken(jwtService.createRefreshToken("useridx", usersDto.getUserIdx()));
 				usersService.addToken(usersDto);
@@ -207,10 +207,7 @@ public class UsersController {
 		try {
 			UsersDto temp = usersService.searchUser(usersDto.getEmail());
 
-			if (temp != null) {// deleted 체크 회원만 반환
-				// usersService.updateMember(usersDto);
-				// 수정 구현 후 추가
-			} else {
+			if (temp == null || temp.isDeleted()) {
 				usersDto.setPassword(encryPassword);
 				usersService.joinUser(usersDto);
 			}
@@ -239,7 +236,6 @@ public class UsersController {
 			} else {
 				return new ResponseEntity<String>(FAIL, HttpStatus.ACCEPTED);
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.debug("checkNickname - 닉네임 체크 중 에러");
