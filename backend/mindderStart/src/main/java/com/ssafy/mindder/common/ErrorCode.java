49fc46f016fed81bfac35aa3c28cc68438e5f712
eunjikim8784 <eunjikim8784@gmail.com>
2023-02-02 17:12:00 +0900

    [Refactor] 공감 등록, 수정, 삭제 api 예외처리 추가
    Former-commit-id: 467c4f9e5977adbfe7074ce9eb2cade2a6d53378

--- backend/mindderStart/src/main/java/com/ssafy/mindder/common/ErrorCode.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/common/ErrorCode.java
@@ -13,11 +13,13 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
 	// 400 Bad Request
-	VALIDATION_EXCEPTION(BAD_REQUEST, "잘못된 요청입니다"),
+	VALIDATION_EXCEPTION(BAD_REQUEST, "잘못된 요청입니다."),
+	VALIDATION_LIKE_EXCEPTION(BAD_REQUEST, "이미 해당 피드에 공감을 눌렀습니다."),
 
 	// 404 NOT FOUND
 	NOT_FOUND_FEED_EXCEPTION(NOT_FOUND, "존재하지 않는 피드입니다."),
 	NOT_FOUND_USER_EXCEPTION(NOT_FOUND, "존재하지 않는 유저입니다."),
+	NOT_FOUND_LIKE_EXCEPTION(NOT_FOUND, "공감을 먼저 등록해주세요."),
 
 	// 500 Internal Server Exception
 	INTERNAL_SERVER_EXCEPTION(INTERNAL_SERVER, "예상치 못한 서버 에러가 발생하였습니다.");
