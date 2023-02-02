c41cc166d428b1bd586edcb91454704e89356155
chanshin0 <windcj1@naver.com>
2023-02-02 10:37:28 +0900

    1. emoTag, emoColor페이지 코드 리팩토링 및 화면 위치 고정 2. 사용하지 않는 파일(mycanvasJs, styleCss) 삭제 3. postDraw페이지 컴포넌트 크기 조정, 이어서 api연결 예정.
    Former-commit-id: a9093aeead64b2570aaf6f6522e63aa9b7f28600

--- frontend/mindder/src/components/post/PostDraw.jsx
+++ frontend/mindder/src/components/post/PostDraw.jsx
@@ -1,44 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 import Canvas from "./Canvas";
-
-import './style.css'
-// import './mycanvas'
-
-const Wrapper = styled.div`
-    /* padding: 0; */
-    /* width: calc(100% - 2rem); */
-    height:31.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    /* border:1px solid blue; */
-`
+import { Wrapper } from "./PostEmotag";
 
 const CrawlingsHere = styled.div`
-  width: 330px;
   height: 71px;
   border: 1px solid #000000;
   border-radius: 6px;
 `
 
 const CanvasDiv = styled.div`
-  width: 330px;
   height: 358px;
   background: #FFFFFF;
-  border: 1px solid #7767FD;
+  /* border: 1px solid #7767FD; */
   border-radius: 19px;
+  position:relative;
+  bottom:2rem;
 `
 
 function PostDraw(props){
   return (
     <Wrapper>
       <CrawlingsHere>여기에 크롤링 결과 출력할거에요</CrawlingsHere>
-      {/* <CanvasDiv> */}
-        <Canvas></Canvas>
-
-      {/* </CanvasDiv> */}
+      <CanvasDiv>
+        <Canvas/>
+      </CanvasDiv>
     </Wrapper>
   );
 };
--- frontend/mindder/src/components/post/PostEmotag.jsx
+++ frontend/mindder/src/components/post/PostEmotag.jsx
@@ -10,7 +10,7 @@ import { Emoticons } from "../../redux/store";
 import { userAction } from "../../redux/store";
 
 export const Wrapper = styled.div`
-    width: calc(100% - 2rem);
+    width: calc(100% - 1rem);
     height:31.5rem;
     display: grid;
     flex-direction: column;
@@ -21,18 +21,20 @@ export const Wrapper = styled.div`
 `;
 
 export const CardContainer = styled.div`
-  width:330px;
+  /* width:330px; */
   display:grid;
-  justify-content: center;
+  justify-items: center;
   align-items:center;
   grid-template-columns: repeat(4, 1fr);
+  padding:0.7rem;
+  column-gap:${(customProps)=>`${customProps.columnGap}rem`};
   /* border: 1px solid black; */
 `
-const EmotionTag = styled.div`
+export const EmotionTag = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 15px;
-  width: 68px;
-  height: 51px;
+  width: 4.3rem;
+  height: 3.3rem;
   /* border: 1px solid black; */
   margin:0.5rem;
   flex-direction:column;
@@ -48,24 +50,24 @@ function PostEmoTag(props) {
 
     const [imgSrc, setImgSrc] = useState(selectedEmo)
 
+    const onClick = (e, emo) => {
+        // currentTarget 사용하면 자식요소클릭을 막고 현재 클릭한 타겟만 안정적으로 잡아준다.
+        const selectedSrc = require(`../../assets/images/face${e.currentTarget.id}.png`)
+        setImgSrc(selectedSrc)
+        dispatch(userAction.SAVE({selected:selectedSrc, case:"imgSrc"}))
+        dispatch(userAction.SAVE({selected:emo.name, case:"emoTag"}))
+    }
     return (
         <Wrapper>
             <EmoHeader text="오늘의 감정은 어떤가요?"/>
             <TodayEmotion
             imgSrc={imgSrc}
             />
-            <CardContainer>
-                {Emoticons.map((emo)=>(
-                    
+            <CardContainer columnGap={0.2}>
+                {Emoticons.map((emo)=>( 
                     <EmotionTag
                     key={emo.id} id={emo.id}
-                    onClick={(e)=>{
-                    // currentTarget 사용하면 자식요소클릭을 막고 현재 클릭한 타겟만 안정적으로 잡아준다.
-                    const selectedSrc = require(`../../assets/images/face${e.currentTarget.id}.png`)
-                    setImgSrc(selectedSrc)
-                    dispatch(userAction.SAVE({selected:selectedSrc, case:"imgSrc"}))
-                    dispatch(userAction.SAVE({selected:emo.name, case:"emoTag"}))
-                    }}
+                    onClick={(e)=>{onClick(e, emo)}}
                     >
                         <EmoTag
                         key={emo.id} emoId={emo.id} emoName={emo.name} 
