e2540278696f6c2c25c2a8ffeaf6e933b154aa21
김민식 <abin7989@gmail.com>
2023-02-02 13:16:36 +0900

    [FEAT]곰돌이 이미지 api
    Former-commit-id: e09be2603b9bca369e9bedbb64a39a0dcdc63932

--- backend/mindderStart/src/main/java/com/ssafy/mindder/file/model/service/FileService.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/file/model/service/FileService.java
@@ -1,9 +1,13 @@
 package com.ssafy.mindder.file.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.ssafy.mindder.file.model.FileDto;
 
 public interface FileService {
-	int addFile(FileDto fileDto);
-	FileDto findFile(int fileIdx);
+	List<FileDto> findNormalBear(Map<String, Integer> map) throws Exception;
+	int addFile(FileDto fileDto) throws Exception;
+	FileDto findFile(int fileIdx) throws Exception;
 }
 
