a86e13d199d7a851c600f077dcab406845443690
abin7989 <abin7989@gmail.com>
2023-02-03 15:35:29 +0900

    [FIX] cros 오류 해결 -2
    Former-commit-id: 4a5eed688ac37828a0856f999c95b23af779122e

--- backend/mindderStart/src/main/java/com/ssafy/mindder/interceptor/JwtInterceptor.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/interceptor/JwtInterceptor.java
@@ -34,9 +34,45 @@ public static final Logger logger = LoggerFactory.getLogger(JwtInterceptor.class
 	public JwtInterceptor(){
 		jwtService = new JwtServiceImpl();
 	}
+	private static final String ORIGIN = "Origin";
+	private static final String AC_REQUEST_METHOD = "Access-Control-Request-Method";
+	private static final String AC_REQUEST_HEADERS = "Access-Control-Request-Headers";
+
+	private static final String AC_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+	private static final String AC_ALLOW_METHODS = "Access-Control-Allow-Methods";
+	private static final String AC_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+
+	private CorsData corsData;
+
+	private String origin;
+	private String allowMethods;
+	private String allowHeaders;
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public void setAllowMethods(String allowMethods) {
+		this.allowMethods = allowMethods;
+	}
+
+	public void setAllowHeaders(String allowHeaders) {
+		this.allowHeaders = allowHeaders;
+	}
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		this.corsData = new CorsData(request);
+
+		if(this.corsData.isPreflighted()) {
+			response.setHeader(AC_ALLOW_ORIGIN, origin);
+			response.setHeader(AC_ALLOW_METHODS, allowMethods);
+			response.setHeader(AC_ALLOW_HEADERS, allowHeaders);
+
+			return false;
+		}
+
 		final String token = request.getHeader(HEADER_AUTH);
 		if(token != null && jwtService.checkToken(token)){
 			logger.info("토큰 사용 가능 : {}", token);
@@ -55,6 +91,49 @@ public static final Logger logger = LoggerFactory.getLogger(JwtInterceptor.class
 		}
 		return false;
 	}
-	
+	class CorsData {
+
+		private String origin;
+		private String requestMethods;
+		private String requestHeaders;
+
+		CorsData(HttpServletRequest request) {
+			this.origin = request.getHeader(ORIGIN);
+			this.requestMethods= request.getHeader(AC_REQUEST_METHOD);
+			this.requestHeaders = request.getHeader(AC_REQUEST_HEADERS);
+		}
+
+		public boolean hasOrigin(){
+			return origin != null && !origin.isEmpty();
+		}
+
+		public boolean hasRequestMethods(){
+			return requestMethods != null && !requestMethods.isEmpty();
+		}
+
+		public boolean hasRequestHeaders(){
+			return requestHeaders != null && !requestHeaders.isEmpty();
+		}
+
+		public String getOrigin() {
+			return origin;
+		}
+
+		public String getRequestMethods() {
+			return requestMethods;
+		}
+
+		public String getRequestHeaders() {
+			return requestHeaders;
+		}
+
+		public boolean isPreflighted() {
+			return hasOrigin() && hasRequestHeaders() && hasRequestMethods();
+		}
+
+		public boolean isSimple() {
+			return hasOrigin() && !hasRequestHeaders();
+		}
+	}
 }
 
