0550992934141daad6846bc5248ca6f971660a90
chanshin0 <windcj1@naver.com>
2023-02-03 17:07:33 +0900

    react-image-webp 라이브러리 설치 및 png파일 webp으로 변환
    Former-commit-id: 0ea2f17fa404b0a917761bb85d5a016a20c525d9

--- frontend/mindder/src/components/post/EmoTag.jsx
+++ frontend/mindder/src/components/post/EmoTag.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import Image from "react-image-webp"
 
-const ImgDiv = styled.img`
-  size:cover;
-  position:relative;
-  top:-3px;
-`
+
 const EmoName = styled.div`
   font-size:x-small;
   font-weight:600;
-  position:relative;
-  top:-10px;
+  position:absolute;
+  margin-top:41px;
 `
 
 function EmoTag(props){
   const imgSrc = require(`../../assets/images/face${props.emoId}.png`)
+  const webpSrc = require(`../../assets/images/face${props.emoId}.webp`)
 
   return(
     <>
-      <ImgDiv src={imgSrc}
-      width={48}
-      height={48}
+      <Image width={48}
+        webp={webpSrc}
+        // src={imgSrc} 
       />
       <EmoName
       >{props.emoName}</EmoName>
