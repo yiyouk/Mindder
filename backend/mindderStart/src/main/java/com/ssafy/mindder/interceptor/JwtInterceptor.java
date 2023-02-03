fbf4fdb11b080648a76f63dc8a9c9a3aaa942a09
김민식 <abin7989@gmail.com>
2023-02-03 15:18:01 +0900

    [FIX] Interceptor Cross 에러 수정
    Former-commit-id: 2a510f8494a5fb721483ae2dfe8d0e78cd5bbc6e

--- backend/mindderStart/src/main/java/com/ssafy/mindder/interceptor/JwtInterceptor.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/interceptor/JwtInterceptor.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,6 +21,7 @@ import com.ssafy.mindder.util.JwtServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RequiredArgsConstructor
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
