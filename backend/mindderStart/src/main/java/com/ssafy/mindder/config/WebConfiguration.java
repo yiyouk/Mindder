e6d2ba61090cf2532f6df52b3abd14c6dbe9f0a9
abin7989 <abin7989@gmail.com>
2023-02-03 17:01:26 +0900

    [FIX] cros 수정
    Former-commit-id: f37f52b9f1c40603a0f8adba6762e9d18c993a04

--- backend/mindderStart/src/main/java/com/ssafy/mindder/config/WebConfiguration.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/config/WebConfiguration.java
@@ -1,18 +1,15 @@
 package com.ssafy.mindder.config;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.ssafy.mindder.interceptor.JwtInterceptor;
-
 import org.springframework.beans.factory.annotation.Value;
+
 @Configuration
 @EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer{
@@ -44,22 +41,6 @@ public class WebConfiguration implements WebMvcConfigurer{
         registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-//	 @Override
-//	    public void addInterceptors(InterceptorRegistry registry) {
-//	        registry.addInterceptor(new JwtInterceptor())
-//	                .addPathPatterns("/**") // 해당 경로에 접근하기 전에 인터셉터가 가로챈다.
-//	                .excludePathPatterns("/users/check-nickname") // 해당 경로는 인터셉터가 가로채지 않는다.
-//	        		.excludePathPatterns("/users/check-email")
-//	        		.excludePathPatterns("/users/email-confirm")
-//	        		.excludePathPatterns("/users/login")
-//	        		.excludePathPatterns("/users/join")
-//	        		.excludePathPatterns("/users/social")
-//	        		.excludePathPatterns("/users/temp-password")
-//	        		.excludePathPatterns("/swagger-resources/**")
-//	        		.excludePathPatterns("/swagger-ui.html")
-//	        		.excludePathPatterns("/v2/api-docs")
-//	        		.excludePathPatterns("/webjars/**");
-//	        		
-//	    }
+
 	
 }
--- backend/mindderStart/src/main/java/com/ssafy/mindder/interceptor/JwtInterceptor.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/interceptor/JwtInterceptor.java
@@ -1,63 +1,41 @@
 package com.ssafy.mindder.interceptor;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.mindder.exception.UnAuthorizedException;
 import com.ssafy.mindder.util.JwtService;
-import com.ssafy.mindder.util.JwtServiceImpl;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
 
 public static final Logger logger = LoggerFactory.getLogger(JwtInterceptor.class);
 	
 	private static final String HEADER_AUTH = "access_token";
-	ObjectMapper objectMapper = new ObjectMapper();
-	final private JwtService jwtService;
-	public JwtInterceptor(){
-		jwtService = new JwtServiceImpl();
-	}
-	
+
+	@Autowired
+	private JwtService jwtService;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
 		final String token = request.getHeader(HEADER_AUTH);
-		response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
-        Map<String, Object> temp = new HashMap<>();
+
 		if(token != null && jwtService.checkToken(token)){
 			logger.info("토큰 사용 가능 : {}", token);
-	        temp.put("status", 401);
-	        temp.put("success", false);
-	        temp.put("message", "유효하지 않은 토큰입니다.");
+			return true;
 		}else{
 			logger.info("토큰 사용 불가능 : {}", token);
-	        temp.put("status", 401);
-	        temp.put("success", false);
-	        temp.put("message", "유효하지 않은 토큰입니다.");
-	        
-	       
+			throw new UnAuthorizedException();
 		}
-		 String result = objectMapper.writeValueAsString(temp);
-	     response.getWriter().write(result);
-		return true;
+
 	}
+	
 }
 
