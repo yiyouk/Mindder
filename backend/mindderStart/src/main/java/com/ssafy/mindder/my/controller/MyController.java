e0d66515d27c1f74e9494811b4ea586092434af7
KimMinShik <abin7989@gmail.com>
2023-02-05 21:12:40 +0900

    [FIX] 조회  accssToken 유효성 검사-4
    Former-commit-id: e6249ba2aac3bb6046886138a5dadb2421e7743d

--- backend/mindderStart/src/main/java/com/ssafy/mindder/my/controller/MyController.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/my/controller/MyController.java
@@ -69,7 +69,7 @@ public class MyController {
 	
 	@ApiOperation(value = "회원 정보 조회 (타인페이지)", notes = "유저 번호에 해당하는 유저 정보를 반환한다.", response = UserInformationDto.class)
 	@GetMapping("/information/{userIdx}")
-	ApiResponse<?> otherUserDetails(//@Value("${file.path.upload-files}") String filePath,
+	ApiResponse<?> otherUserDetails(@Value("${file.path.upload-files}") String filePath,
 			@PathVariable("userIdx") @ApiParam(value = "유저 번호", required = true) int userIdx ,@RequestHeader("access_token") String accessToken) {
 		logger.debug("otherUserDetails - 호출");
 		try {
