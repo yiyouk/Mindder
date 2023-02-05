84cddc2f5516d9df5c49c2524c34989dfb46bb3b
이유경 <yiyou00@naver.com>
2023-02-05 11:02:56 +0900

    [FEAT]#135 - api 연결 : 게시글 상세 페이지 2차
    Former-commit-id: f4f117851c1863beb367faa54dacfaa4eef036f4

---
+++ frontend/mindder/src/components/feed/BookMark.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+
+function BookMark(props) {
+ 
+  return (
+    <div>
+        {/* <BsBookmark color="#7767FD"/> */}
+        <BsBookmarkFill size="20" color="#7767FD"/>
+    </div>
+
+  );
+};
+
+export default BookMark;
