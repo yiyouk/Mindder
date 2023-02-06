37a34bb59b3d084b34680250b9fe4162fc52d46e
이유경 <yiyou00@naver.com>
2023-02-06 17:51:20 +0900

    [FEAT]#150 - api 연결  : 게시글 상세 페이지 완료
    Former-commit-id: dd7a852ee642505c5b635d7893d09fbf25c2401b

---
+++ frontend/mindder/src/components/feed/Scraps.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+
+import api from "../../api/api";
+
+function Scraps({myScrap, feedIdx}) {
+  const navigate = useNavigate();
+  const [scrap, setScrap] = useState(myScrap);
+
+  //정보 가져오기
+  // useEffect(()=>{
+      
+  // }, [scrap])
+
+  //스크랩등록
+  const scrapRegister = async() => {
+    try {
+      console.log(feedIdx)
+      const response = await api.post(`/scraps/${feedIdx}`);
+      
+      setScrap(!scrap);
+      console.log(response);
+      
+    } catch (e) {
+      alert("오류 발생!");
+      console.error(e);
+      navigate("/error");
+    }
+  }
+
+  //스크랩 삭제
+  const scrapDelete = async() => {
+    console.log(feedIdx)
+    try {
+      const response = await api.delete(`/scraps/${feedIdx}`);
+      
+      setScrap(!scrap);
+      console.log(response);
+      
+    } catch (e) {
+      alert("오류 발생!");
+      console.error(e);
+      navigate("/error");
+    }
+  }
+
+
+
+  return (
+    <div>
+        {scrap ? <BsBookmarkFill onClick={scrapDelete}  size="20" color="#7767FD"/> : <BsBookmark onClick={scrapRegister} size="20" color="#7767FD"/>}
+    </div>
+
+  );
+};
+
+export default Scraps;
