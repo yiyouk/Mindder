82cb6a1886ea36482f551ab53857da40faf5f2c9
eunji <eunjikim8784@gmail.com>
2023-02-07 23:50:13 +0900

    [FEAT] #163 통계 조회 api 구현
    Former-commit-id: db296b07decf36c7511f86128ef60ecdb207567a

---
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/statistics/model/StatisticsDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.mindder.statistics.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatisticsDto {
+
+	private int emoteIdx;
+	private String emoteColorTag;
+	private String updateDate;
+
+}
