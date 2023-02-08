156a6559d8f77925169920340aa03a9284d57af1
syhj0326 <syhj0326@naver.com>
2023-02-08 12:33:04 +0900

    [FEAT]: 스크랩페이지 API 구현, 에러페이지 디자인
    Former-commit-id: 8da0873128ca115df696e35d23062c062d9cc22d

--- frontend/mindder/src/components/user/ScrapFeedsList.jsx
+++ frontend/mindder/src/components/user/ScrapFeedsList.jsx
@@ -1,56 +1,86 @@
 // 추천 캔버스 출력할 컴포넌트
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-import ScrapFeedsItem from "./ScrapFeedsItem";
-import ProfileImage from "../../commons/ui/ProfileImage";
+import Profile from "../../commons/ui/Profile";
+import CanvasItem from "../../commons/list/CanvasItem";
 
+import api from "../../api/api";
 
 const Wrapper = styled.div`
-      width: 100%;
       align-items: center;
       justify-content: center;
       
-      width: 360px;
-      max-width: 720px;
-    /* & > * {
-        :not(:last-child) {
-            margin-bottom: 16px;
-        }
-    } 
-    border : 1px solid black;
-    background-color:#7767FD;
-    border-radius:20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction:column; */
+
 `;
 
 
 const ScrapFeedsItemContainer = styled.div`
-    /* border : 1px solid black; */
-    width:100%;
+    margin-bottom: 2rem;
     display: grid;
     grid-template-columns: 1fr;
     align-items: center;
     justify-content: center;
-
+`
+const ProfileContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &>span{
+       font-size : 0.8rem;
+       color: grey;
+       margin-top: 1rem;
+    }
 `
 
 function ScrapFeedsList(props){
-  return (
+  const navigate = useNavigate();
+  const [scrapFeeds, setScrapFeeds] = useState([]);
+
+  useEffect(() => {
+      getScrapFeeds();
+  }, [])
+
+  // 스크랩 글 가져오기
+  const getScrapFeeds = async() => {
+      try {
+          const response = await api.get(`/scraps/my`);
+          if(response.data.success){
+              console.log(response.data.data);
+              setScrapFeeds(response.data.data)
+          
+          } else {
+              alert("데이터를 조회하지 못했습니다.");
+          }
+
+      } catch (e) {
+          console.error(e);
+          navigate("/error");
+      }
+  }
+return (
+
     <Wrapper>
-      <ScrapFeedsItemContainer>
-        {/* 여기서 for문 */}
-        <ScrapFeedsItem/>
-        <ScrapFeedsItem/>
-        <ScrapFeedsItem/>
-        <ScrapFeedsItem/>
-      </ScrapFeedsItemContainer>
+        { !scrapFeeds || scrapFeeds.length === 0? ( 
+            <div>저장한 글이 없습니다.</div>   
+        ):(
+            <>
+                {scrapFeeds.map((feeds, idx) => (
+                    <ScrapFeedsItemContainer>
+                        <ProfileContainer>
+                            <Profile imgsize='s' userIdx={feeds.userIdx} namesize='s' name={feeds.nickname}/>
+                            <span>{feeds.updateDate}</span>
+                        </ProfileContainer>
+                        <CanvasItem size='ml' list={feeds} up={true} />
+                    </ScrapFeedsItemContainer>
+                    ))
+                }
+            </>
+        )}
     </Wrapper>  
-  );
+);
 }
+
 
 export default ScrapFeedsList;
