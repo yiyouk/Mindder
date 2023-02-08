9e3fffaa7c37361c895c554d848ab6aed1610c87
eunjikim8784 <eunjikim8784@gmail.com>
2023-02-08 09:38:10 +0900

    [FIX] 통계 api 이미지 추가
    Former-commit-id: 5b93af1db56a8de686908b1b008c9a69225cc5fe

--- backend/mindderStart/src/main/java/com/ssafy/mindder/statistics/controller/StatisticsController.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/statistics/controller/StatisticsController.java
@@ -1,10 +1,12 @@
 package com.ssafy.mindder.statistics.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,6 +17,7 @@ import com.ssafy.mindder.common.ErrorCode;
 import com.ssafy.mindder.common.SuccessCode;
 import com.ssafy.mindder.common.dto.ApiResponse;
 import com.ssafy.mindder.feeds.controller.FeedsController;
+import com.ssafy.mindder.file.model.service.FileService;
 import com.ssafy.mindder.statistics.model.StatisticsDto;
 import com.ssafy.mindder.statistics.model.service.StatisticsService;
 
@@ -29,6 +32,11 @@ public class StatisticsController {
 
 	@Autowired
 	private StatisticsService statisticsService;
+	@Autowired
+	private FileService fileService;
+
+	@Value("${file.path.upload-files}")
+	private String filePath;
 
 	private static final Logger logger = LoggerFactory.getLogger(FeedsController.class);
 
@@ -39,6 +47,11 @@ public class StatisticsController {
 		logger.debug("statisticsList - 호출");
 		try {
 			List<StatisticsDto> statistics = statisticsService.findStatistics();
+			for (int i = 0; i < statistics.size(); i++) {
+				Map<String, String> file = fileService.findFile(statistics.get(i).getFileIdx(), filePath);
+				statistics.get(i).setBase64(file.get("base64"));
+				statistics.get(i).setExtension(file.get("extension"));
+			}
 			return ApiResponse.success(SuccessCode.READ_STATISTICS_LIST, statistics);
 		} catch (Exception e) {
 			e.printStackTrace();
