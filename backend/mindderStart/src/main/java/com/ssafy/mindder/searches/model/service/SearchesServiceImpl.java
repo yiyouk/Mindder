11fd082398b0fb768e33b0bc6d938bf39327cb63
abin7989 <abin7989@gmail.com>
2023-02-09 14:19:47 +0900

    [FIX] 파이어베이스 삭제
    Former-commit-id: 06e0d85146aa95d5c5df55458ae498ac1586ef79

--- backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/service/SearchesServiceImpl.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/service/SearchesServiceImpl.java
@@ -16,9 +16,7 @@ public class SearchesServiceImpl implements SearchesService {
 	SearchesMapper searchesMapper;
 	@Override
 	public SearchesDto find(String word) throws Exception {
-		
-		return new SearchesDto(searchesMapper.searchUser(word),
-				searchesMapper.searchNomal(word));
+		return null;
 	}
 	@Override
 	public List<UsersDto> findUser(String word) throws Exception {
