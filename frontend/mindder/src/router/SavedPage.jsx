a8606d2162d008e36a3b9680b60605c182168e2d
양희진 <syhj0326@naver.com>
2023-02-09 17:21:24 +0900

    [FEAT] #172 달력 구현, 에러페이지, 일반태그
    Former-commit-id: fcefaf0271eb1d930eec062f167429d111dc2015

--- frontend/mindder/src/router/SavedPage.jsx
+++ frontend/mindder/src/router/SavedPage.jsx
@@ -1,22 +1,41 @@
 // 라우터 폴더는 uri기준으로 각각 파일 작성
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import {IoArrowBackCircle} from "react-icons/io5";
 
 import ScrapFeedsList from "../components/user/ScrapFeedsList";
 
 const Wrapper = styled.div`
     /* padding: 16px; */
-    /* width: 100vw; */
+    width: 100vw;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    /* border: 1px solid blue; */
+
+`;
+const BtnContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    margin-left: 1rem;
 `;
 
+const ScrapContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 function SavedPage(props) {
+    
+  const navigate = useNavigate();
     return (
         <Wrapper>
-            <ScrapFeedsList></ScrapFeedsList>
+            <BtnContainer>
+                <IoArrowBackCircle color="#7767FD" size="40" onClick={() => navigate(-1)}/>
+            </BtnContainer>
+            <ScrapContainer>
+                <ScrapFeedsList/>
+            </ScrapContainer>
         </Wrapper>
     );
 }
