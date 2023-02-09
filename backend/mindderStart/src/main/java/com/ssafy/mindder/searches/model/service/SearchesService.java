f658a18bb1fcc621ca46979c94751618b8ea288f
abin7989 <abin7989@gmail.com>
2023-02-09 14:42:57 +0900

    [FEAT] 유저 검색
    Former-commit-id: 141206975bed19fd3cd771959fb031679296f74f

--- backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/service/SearchesService.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/service/SearchesService.java
@@ -1,7 +1,11 @@
 package com.ssafy.mindder.searches.model.service;
 
+import java.util.List;
+
 import com.ssafy.mindder.searches.model.SearchesDto;
+import com.ssafy.mindder.users.model.UsersDto;
 
 public interface SearchesService {
 	SearchesDto find(String word) throws Exception;
+	List<UsersDto> findUser(String word) throws Exception;
 }
