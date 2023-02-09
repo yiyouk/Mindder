bab9fbca4a5b3e1e842c8179b7354ddf47f4f992
신동찬 <windcj1@naver.com>
2023-02-09 14:25:59 +0900

    [FIX] #177 유저페이지 첫화면 완료
    Former-commit-id: a5c4e108bab166ddd5273f2b1940a8bbc8570602

--- frontend/mindder/src/redux/store.js
+++ frontend/mindder/src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from './reducers';
-// import {tokenSlice} from './Auth';
 
 export const store = configureStore({
   reducer:rootReducer
