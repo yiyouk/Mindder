2c0ff2374d448cd830aae1b7adddf75d18ffe777
eunjikim8784 <eunjikim8784@gmail.com>
2023-02-10 17:31:44 +0900

    [FIX] 도서 크롤링 로직 수정
    Former-commit-id: 1d87c43abb891ada37d05d8cd566638451a859d0

--- backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/service/SearchesService.java
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/service/SearchesService.java
@@ -2,7 +2,6 @@ package com.ssafy.mindder.searches.model.service;
 
 import java.util.List;
 
-import com.ssafy.mindder.feeds.model.FeedsDto;
 import com.ssafy.mindder.searches.model.SearchesDto;
 import com.ssafy.mindder.users.model.UsersDto;
 
@@ -10,4 +9,5 @@ public interface SearchesService {
 	SearchesDto find(String word) throws Exception;
 	List<UsersDto> findUser(String word) throws Exception;
 	List<String> findHash(String word) throws Exception;
+	String findKeyword(int userIdx) throws Exception;
 }
