975ffd260d445b354d65ad54896366b2fb5c3277
eunjikim8784 <eunjikim8784@gmail.com>
2023-02-10 16:07:15 +0900

    [FEAT] #194 도서 목록 조회(크롤링) api 구현
    Former-commit-id: 5df52fc98f544cc6f42b76a90cc55232d9f2eb9b

---
+++ backend/mindderStart/src/main/java/com/ssafy/mindder/searches/model/BooksDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.mindder.searches.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BooksDto {
+	private String title;
+	private String link;
+	private String image;
+}
