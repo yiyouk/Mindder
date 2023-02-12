1f14f0bbe3f40d1568ff84f397eead025de17827
신동찬 <windcj1@naver.com>
2023-02-12 23:32:10 +0900

    [Feat] : 카카오 소셜로그인 프론트 구현, 스크랩페이지 프로필이미지 출력
    Former-commit-id: 079de510ace4109c31971a2c3e529039216757f1

---
+++ frontend/mindder/src/social/OAuth.jsx
@@ -0,0 +1,6 @@
+// 카카오 로그인 키
+
+const CLIENT_ID = "0d2c29aab3ff2236ab9808ecb73e15e7";
+const REDIRECT_URI =  "http://localhost:3000/oauth/callback/kakao";
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
