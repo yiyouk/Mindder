d08500999a6c3d6e3980de39867f0c8818439e48
chanshin0 <windcj1@naver.com>
2023-02-15 14:40:25 +0900

    [FIX] : 픽스
    Former-commit-id: da6427b649da77a8cc968c128ee4ffe750542483

--- frontend/mindder/src/social/KakaoAuthRedirect.jsx
+++ frontend/mindder/src/social/KakaoAuthRedirect.jsx
@@ -4,13 +4,34 @@ import React, {useEffect} from "react";
 import { useDispatch } from "react-redux";
 import api from "../api/api"
 import axios from "axios";
+import { CLIENT_ID } from "./OAuth";
 
 function KakaoAuthRedirect  (props) {
   // const dispatch = useDispatch();
 
   // 인가코드
-  // let code = new URL(window.location.href).searchParams.get('code')
-  // console.log(code)
+  let code = new URL(window.location.href).searchParams.get('code')
+  console.log(code)
+
+  const getToken = async(code)=>{
+    try {
+      axios.post(`https://kauth.kakao.com/oauth/token?
+          grant_type="authorization_code"
+          &client_id=${CLIENT_ID}
+          &redirect_uri=http://localhost:3000/oauth/callback/kakao
+          &code=${code}`
+          , {
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+      }})
+      .then((res) => {
+      console.log(res)  
+      })
+    } catch (error) {
+      
+    }
+    
+  }
 
   // const kakaoLogin = async()=>{
   //   try {
@@ -22,10 +43,12 @@ function KakaoAuthRedirect  (props) {
   //   }
   // }
 
-  // useEffect( () => {
+
+
+  useEffect( () => {
     // kakaoLogin()
     // getToken(code)
-  // }, []);
+  }, []);
 
   return (
     <>
