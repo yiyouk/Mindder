dcb2a00f193fd9da476b59e891f12d984df8a476
syhj0326 <syhj0326@naver.com>
2023-02-16 18:58:31 +0900

    [FEAT] 알림 전체삭제 구현, 알림리스트에 피드 이미지 삽입
    Former-commit-id: 9d1fdca25c4e9834bd97b594f8786ba5c37d6b98

--- frontend/mindder/src/commons/list/AlarmItem.jsx
+++ frontend/mindder/src/commons/list/AlarmItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
+import { useSelector, useDispatch } from "react-redux";
 import ProfileImage from "../../commons/ui/ProfileImage";
 
 import dayjs from 'dayjs';
 import api from "../../api/api";
+import { AlarmCheck } from "../bar/HeaderBar";
 
 const Wrapper = styled.div`
     display: flex;
@@ -29,16 +30,18 @@ const SideContainer = styled.div`
     justify-content: flex-end;
 `;
 
-const ContentText = styled.span`
-    max-width: 18rem;
+const ContentText = styled.div`
+    max-width: 17rem;
     color: rgb(67, 67, 67);
     font-size: 0.75rem;
     padding: 0rem 0.2rem 0rem 0rem;
-    &>span{
-        font-weight: bold;
-        color: #7767FD;
-    }
+    display:flex;
 `;
+const TextDeco = styled.span`
+    font-weight: bold;
+    color: #7767FD;
+    cursor: pointer;
+`
 const DelAlarm = styled.div`
     font-size: 0.8rem;
     color: rgb(67, 67, 67);
@@ -49,27 +52,25 @@ const Date = styled.span`
     color: rgb(67, 67, 67);
     margin-bottom: 0.1rem;
 `
-const M = styled.div`
-    width:5px;
-`
-const AlertCheck = styled.div`
-    width:5px;
-    height:5px ;
-    border-radius: 50%;
-    background-color: #7767FD;
-    z-index: 3;
-    
-`
+
 const Container = styled.div`
     display: flex;
     background: white;
+    
+`
+const ImgStyle = styled.img`
+  width: 2rem;
+  height: 2rem;
+  margin: 0.2rem;
+  border: 0.02rem solid rgba(219, 219, 219, 0.3);
+  box-shadow: 0.1rem 0.1rem 0.1rem rgba(219, 219, 219, 0.3);
+  border-radius: 4px;
+  position: relative;
+  cursor: pointer;
 `
 
 function AlarmItem({alarm, userIdx, getData, alarmCount}) {
     const navigate = useNavigate();
- 
-    
-    // 닉네임 클릭시 해당 유저 페이지 이동
     const onClick = () => {
         navigate(`/f/${alarm.feedIdx}`);
         removeAlarm()
@@ -104,9 +105,9 @@ function AlarmItem({alarm, userIdx, getData, alarmCount}) {
             <Wrapper>
                 <Container>
                     {alarm.read === false?                        
-                        <AlertCheck></AlertCheck>
+                        <AlarmCheck></AlarmCheck>
                         :
-                        <M/>
+                        null
                     }
                     <div  onClick={removeAlarm}>
                         <ProfileImage extension={alarm.extension} size = "s" userIdx={userIdx} imgSrc={alarm.base64}/>
@@ -117,9 +118,22 @@ function AlarmItem({alarm, userIdx, getData, alarmCount}) {
                                 <Date>{dayjs(alarm.updateDate).get('year')}년 {dayjs(alarm.updateDate).get('month') + 1}월 {dayjs(alarm.updateDate).get('date')}일</Date>
                         </SideContainer>
                         {{ 
-                            1:<ContentText>{alarm.nickname}님이 회원님을 팔로우합니다.</ContentText>,
-                            2:<ContentText>{alarm.nickname}님이 회원님의 <span onClick={onClick}>게시글</span>에 댓글을 남겼습니다.</ContentText>,
-                            3:<ContentText>{alarm.nickname}님이 회원님의 <span onClick={onClick}>게시글</span>에 감정을 표현했습니다.</ContentText>
+                            1:<>
+                                <ContentText>{alarm.nickname}님이 회원님을 팔로우합니다.</ContentText>
+                              </>,
+                            2:<>
+                                <ContentText>
+                                    <div>{alarm.nickname}님이 회원님의 <TextDeco onClick={onClick}>게시글</TextDeco>에 댓글을 남겼습니다.</div>
+                                    <ImgStyle src={"data:image/" + alarm.feedExtension + ";base64," + alarm.feedBase64} onClick={onClick}/>
+                                </ContentText>
+                                
+                              </>,
+                            3:<>
+                                <ContentText>
+                                    <div>{alarm.nickname}님이 회원님의 <TextDeco onClick={onClick}>게시글</TextDeco>에 감정을 표현했습니다.</div> 
+                                    <ImgStyle src={"data:image/" + alarm.feedExtension + ";base64," + alarm.feedBase64} onClick={onClick}/>
+                                </ContentText>
+                              </>
                         }[alarm.alarmType]}
             
                     </Main>
