9f9822851ccc2955f6b32afa84a2d82b79aa1489
이유경 <yiyou00@naver.com>
2023-02-16 21:53:33 +0900

    [FIX]#272 - 여러분의 벽돌
    Former-commit-id: d1fa716887cc16a4bdb28651f65ffcebaad710a1

--- frontend/mindder/src/components/feed/CommentList.jsx
+++ frontend/mindder/src/components/feed/CommentList.jsx
@@ -16,7 +16,6 @@ const Wrapper = styled.div`
         color: gray;
         margin-bottom: 1rem;
     }
-    /* border:1px solid; */
 `;
 
 function CommentList({feedIdx, commentCount}) {
@@ -34,12 +33,11 @@ function CommentList({feedIdx, commentCount}) {
     }
 
     //댓글 데이터 가져오기
-    const getComments = async() => { // async, await을 사용하는 경우
+    const getComments = async() => {
         try {
             const response = await api.get(`/comments/${feedIdx}`);
-            console.log(response.data)
+
             if(response.data.success){
-                console.log(response.data.data);
                 setComments(response.data.data)
             } else {
                 console.log("조회 실패")
