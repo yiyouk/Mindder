40996873efed38429d33191e9d749c60f826d2c0
이유경 <yiyou00@naver.com>
2023-02-16 19:34:21 +0900

    [FEAT]#255 - 프론트 푸시알람 구현 4차
    Former-commit-id: a958ec2f668d5c85e39343e68ab28f0198e16248

--- frontend/mindder/public/firebase-messaging-sw.js
+++ frontend/mindder/public/firebase-messaging-sw.js
@@ -12,3 +12,20 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+//백그라운드 서비스워커 설정
+messaging.onBackgroundMessage(messaging, (payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: payload,
+    icon: "/firebase-logo.png",
+  };
+  
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
